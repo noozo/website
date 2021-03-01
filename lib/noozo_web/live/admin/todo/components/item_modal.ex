95336e9eb711fd66b3f910dd1f95aacd306d967b
Pedro Assunção <assuncas@gmail.com>
2021-03-01 09:18:03 +0000

    Flex wraping todo item labels in modal

--- lib/noozo_web/live/admin/todo/components/item_modal.ex
+++ lib/noozo_web/live/admin/todo/components/item_modal.ex
@@ -14,12 +14,6 @@ defmodule NoozoWeb.Admin.Todo.Components.ItemModal do
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div class="fixed inset-0 transition-opacity"
              x-show="modalOpen"
-             x-transition:enter="ease-out duration-300"
-             x-transition:enter-start="opacity-0"
-             x-transition:enter-end="opacity-100"
-             x-transition:leave="ease-in duration-200"
-             x-transition:leave-start="opacity-100"
-             x-transition:leave-end="opacity-0"
              aria-hidden="true"
              phx-click="item_clicked"
              phx-value-draggable_id="">
@@ -30,12 +24,6 @@ defmodule NoozoWeb.Admin.Todo.Components.ItemModal do
 
         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
               x-show="modalOpen"
-              x-transition:enter="ease-out duration-300"
-              x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-              x-transition:leave="ease-in duration-200"
-              x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-              x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             role="dialog" aria-modal="true" aria-labelledby="modal-headline">
           <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start gap-8">
